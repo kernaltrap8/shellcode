07f4b015b93fac03f3e7971b00e4af1ceb7f945e
kernaltrap8 <kernaltrap@gmail.com>
2024-06-11 12:23:18 -0500

    add files
    Signed-off-by: kernaltrap8 <kernaltrap@gmail.com>

--- src/shellcode.s
+++ src/shellcode.s
@@ -1,17 +1,6 @@
-section .data
-	dividend dq 100
-	divisor  dq 50
-
 section .text
 global _start
 	_start:
-		mov rax, qword [dividend]
-		mov edx, 0
-		mov eax, dword [divisor]
-
-		div eax
-		ret
-
 		mov rax, 0x25
 		mov rbx, 0x10
 
